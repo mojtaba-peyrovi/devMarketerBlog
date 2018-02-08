73ec403551a1a9efa8eadc80312a1006de71fdf9
= <mojtaba.peyrovi@gmail.com>
2018-02-08 15:40:48 +0700

    testing if my commit is added to contributions

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -17,7 +17,7 @@ class postController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('created_at','desc')->paginate(10);
+        $posts = Post::orderBy('created_at','desc')->paginate(15);
         return view('posts.index',compact('posts'));
     }
 
